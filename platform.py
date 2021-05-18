ad8fc931a410344c09b400a2369ddbd927447a5d
qitas <qitas@qitas.cn>
2021-05-18 11:58:30 +0800

    fix

--- platform.py
+++ platform.py
@@ -14,11 +14,6 @@ class P115Platform(PlatformBase):
             "board_build.core", self.board_config(variables.get("board")).get(
                 "build.core", "arduino"))
 
-
-            if build_core in ("MegaCoreX", "megatinycore"):
-                self.packages["toolchain-atmelavr"]["version"] = "~2.70300.0"
-                self.packages["tool-avrdude-megaavr"]["version"] = "~2.60300.0"
-
         if any(t in targets for t in ("fuses", "bootloader")):
             self.packages["tool-avrdude-megaavr"]["optional"] = False
 
